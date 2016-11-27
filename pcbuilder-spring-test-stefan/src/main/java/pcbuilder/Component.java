91f6c56e40d75f98f44b4975371ef0caff258dee
Daniel de Witte <danieldewitte@gmail.com>
2016-11-27 17:45:24 +0100

    Added test to see if rest service takes Classes as parameters, including soapUI test

--- pcbuilder-spring-test-stefan/src/main/java/pcbuilder/Component.java
+++ pcbuilder-spring-test-stefan/src/main/java/pcbuilder/Component.java
@@ -3,12 +3,23 @@ package pcbuilder;
 public class Component {
 
     private final int _id;
+    private final String name;
 
-    public Component(int id) {
+    public Component() {
+        this._id = 10;
+        this.name = "Piet Patat";
+    }
+
+    public Component(int id, String name) {
         this._id = id;
+        this.name = name;
     }
 
     public int getId() {
         return _id;
+    }
+
+    public String getName() {
+        return name;
     }
 }
--- pcbuilder-spring-test-stefan/src/main/java/pcbuilder/Configurator.java
+++ pcbuilder-spring-test-stefan/src/main/java/pcbuilder/Configurator.java
@@ -1,5 +1,6 @@
 package pcbuilder;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class Configurator {
 
     @RequestMapping("/getcomponent")
-    public Component getComponent(@RequestParam(value="id", defaultValue="0") int id) {
-        return new Component(id);
+    public Component getComponent(@RequestParam(value="id", defaultValue="0") int id, @RequestParam(value="name", defaultValue="Pietje Puk") String name) {
+        return new Component(id, name);
+    }
+
+    @RequestMapping("/putcomponent")
+    public Component putComponent(@RequestBody Component component) {
+        return component;
     }
 
 }
